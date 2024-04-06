5060e87cacb6938d3b78e0bfc1a682f72d86667b
Bouhdiallo <bouhdiallo22@gmail.com>
2024-04-06 13:10:34 +0000

    bouton j'aime rendu dynamique

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit{
+titreArticle: string =  "titre de l'article";
+prixArticle: number = 12;
+textAltImg: string= "titre alternative de l'image";
+urlImg: string = "assets/bike-909690_1280.jpg";
+TotaLike: number = 0;
+
+   constructor(){}
+  ngOnInit(): void {
+  }
+  onlike(){
+    this.TotaLike++;
+  }
 
 }
